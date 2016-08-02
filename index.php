8eaba34b24212add26d986294ac853ac04b1828f
Turing1887 <tim.zschage@t-online.de>
2016-08-02 17:16:49 +0200

    add rootfiles

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php ?>
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Football Manager</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+
+        <link rel="stylesheet" href="css/styles.css">
+
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+
+
+        <nav class="main-nav">
+          <ul class="main-menu">
+            <li>home</li>
+            <li>calendar</li>
+            <li>squad</li>
+            <li>tactics</li>
+            <li>transfer</li>
+          </ul>
+        </nav>
+        <section></section>
+
+
+
+
+
+
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+
+    </body>
+</html>
